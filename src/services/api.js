8dc4422c6a2c74637761b97c0767738f44c86407
Yafet Tegbaru <48455231+yafet188@users.noreply.github.com>
2025-03-01 23:37:18 -0500

    Deleted api.js for privacy reasons.

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,1 @@
-const API_KEY = "56558bbaee32b1dd686c02f6a96623a6";
-const BASE_URL = "https://api.themoviedb.org/3";
 
-export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data = await response.json()
-    return data.results
-};
-
-export const searchMovies = async (query) => {
-    const response = await fetch(
-        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = await response.json()
-    return data.results
-};
